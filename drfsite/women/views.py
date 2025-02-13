3025abc10d2a1c44df1d10ebb835d44d2385eddb
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-02-13 20:34:26 +0300

    Закончил введение в сериализацию

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -14,13 +14,17 @@ from .serializers import WomenSerializer
 
 class WomenAPIView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data}) # many=True, говорит сериализатору о том, что он
+        # должен обрабатывать не одну запись, а список записей и выдавать список записей.
 
     def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = Women.objects.create(
             title = request.data['title'],
             content = request.data['content'],
             cat_id = request.data['cat_id']
         )
-        return Response({'cat_id': model_to_dict(post_new)})
+        return Response({'cat_id': WomenSerializer(post_new).data})
